189fa611313f583b5c1fdc9fbfda3d1d193fc614
Felipe Fernando Corrêa <160677520+FelipeFernando04@users.noreply.github.com>
2025-09-02 20:46:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-// Espera todo o conteúdo da página carregar
 document.addEventListener('DOMContentLoaded', function() {
+    const cakeContainer = document.getElementById('cakeContainer');
+    const closeInfoButton = document.getElementById('closeInfo');
 
-    // Encontra os elementos pelo ID que demos no HTML
-    const botaoVerDetalhes = document.getElementById('botaoVerDetalhes');
-    const detalhesFesta = document.getElementById('detalhesFesta');
-    const botaoFechar = document.getElementById('botaoFechar');
-
-    // O que acontece quando clicar no botão "Clique aqui"
-    botaoVerDetalhes.addEventListener('click', function() {
-        detalhesFesta.classList.add('visivel'); // Mostra os detalhes
+    cakeContainer.addEventListener('click', function() {
+        cakeContainer.classList.toggle('flipped');
     });
 
-    // O que acontece quando clicar no botão "Fechar"
-    botaoFechar.addEventListener('click', function() {
-        detalhesFesta.classList.remove('visivel'); // Esconde os detalhes
+    closeInfoButton.addEventListener('click', function(event) {
+        event.stopPropagation(); 
+        cakeContainer.classList.remove('flipped');
     });
-
 });
